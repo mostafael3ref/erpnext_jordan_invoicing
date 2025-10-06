dd7182cfe8efed83ee1334d7c4ed688126da8fd2
Mostafa ELareef <121424728+mostafael3ref@users.noreply.github.com>
2025-10-06 10:44:41 +0300

    Update install.py

--- erpnext_jofotara/install.py
+++ erpnext_jofotara/install.py
@@ -1,15 +1,15 @@
 import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
-def after_install():
-    custom_fields = {
+def after_migrate():
+    if not frappe.db.exists("DocType", "Sales Invoice"):
+        return
+    fields = {
         "Sales Invoice": [
-            dict(fieldname="jofotara_status", label="JoFotara Status", fieldtype="Select",
-                 options="\nPending\nSent\nFailed", insert_after="naming_series"),
-            dict(fieldname="jofotara_uuid", label="JoFotara UUID", fieldtype="Data",
-                 insert_after="jofotara_status", read_only=1),
-            dict(fieldname="jofotara_qr", label="JoFotara QR", fieldtype="Small Text",
-                 insert_after="jofotara_uuid", read_only=1),
+            dict(fieldname="jofotara_status", label="JoFatora Status",
+                 fieldtype="Select", options="\nPending\nSubmitted\nError", insert_after="naming_series"),
+            dict(fieldname="jofotara_uuid", label="JoFatora UUID", fieldtype="Data", read_only=1),
+            dict(fieldname="jofotara_qr", label="JoFatora QR", fieldtype="Small Text", read_only=1),
         ]
     }
-    create_custom_fields(custom_fields, ignore_validate=True)
+    create_custom_fields(fields, ignore_validate=True)
