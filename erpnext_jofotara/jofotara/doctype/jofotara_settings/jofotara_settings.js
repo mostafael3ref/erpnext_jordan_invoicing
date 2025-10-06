3bf8d38219becd645e48e08ee7a5a02bfedd139c
Mostafa ELareef <121424728+mostafael3ref@users.noreply.github.com>
2025-10-06 16:06:38 +0300

    Update jofotara_settings.js

--- erpnext_jofotara/jofotara/doctype/jofotara_settings/jofotara_settings.js
+++ erpnext_jofotara/jofotara/doctype/jofotara_settings/jofotara_settings.js
@@ -1,7 +1,1 @@
-frappe.ui.form.on('JoFotara Settings', {
-  refresh(frm) {
-    frm.add_custom_button(__('Open in Desk'), () => {
-      window.location.href = '/app/jo-fotara-settings';
-    });
-  }
-});
+
